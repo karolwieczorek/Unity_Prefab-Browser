f16cd9ca555c0e31bbd573fbc69d7c843d4653e2
Karl <karolwieczorek9@o2.pl>
2018-02-16 00:32:30 +0100

    basic prefab browser

---
+++ PrefabBrowser/Editor/PrefabBrowserTreeView.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+namespace HypnagogiaGames.PrefabBrowser
+{
+    public class PrefabBrowserTreeView : TreeView
+    {
+        class PrefabViewItem : TreeViewItem
+        {
+            public GameObject gameObject;
+        }
+
+        PrefabViewItem root;
+        List<PrefabViewItem> allItems;
+        private GameObject targetObject;
+
+        public PrefabBrowserTreeView(TreeViewState treeViewState)
+            : base(treeViewState)
+        {
+            Reload();
+        }
+
+        protected override TreeViewItem BuildRoot()
+        {
+            // BuildRoot is called every time Reload is called to ensure that TreeViewItems 
+            // are created from data. Here we create a fixed set of items. In a real world example,
+            // a data model should be passed into the TreeView and the items created from the model.
+
+            // This section illustrates that IDs should be unique. The root item is required to 
+            // have a depth of -1, and the rest of the items increment from that.
+
+            if (targetObject != null)
+            {
+                int id = 0;
+                root = new PrefabViewItem { id = id++, depth = -1, displayName = "Root" };
+                allItems = new List<PrefabViewItem>();
+
+                allItems.Add(new PrefabViewItem { id = id++, depth = 0, displayName = targetObject.name, gameObject = targetObject });
+                AddChildren(id, 1, targetObject.transform);
+            }
+            else
+            {
+                root = new PrefabViewItem { id = 0, depth = -1, displayName = "No object selected" };
+                allItems = new List<PrefabViewItem>();
+            }
+
+            // Utility method that initializes the TreeViewItem.children and .parent for all items. 
+            SetupParentsAndChildrenFromDepths(root, allItems.Select(x => (TreeViewItem)x).ToList());
+
+            // Return root of the tree
+            return root;
+        }
+
+        private int AddChildren(int id, int depth, Transform parent)
+        {
+            if (parent.childCount <= 0)
+                return id;
+
+            foreach (Transform child in parent)
+            {
+                allItems.Add(new PrefabViewItem { id = id++, depth = depth, displayName = child.name, gameObject = child.gameObject });
+
+                id = AddChildren(id, depth + 1, child);
+            }
+
+            return id;
+        }
+
+        internal void SetTarget(GameObject targetObject)
+        {
+            this.targetObject = targetObject;
+            Reload();
+        }
+
+        protected override void SelectionChanged(IList<int> selectedIds)
+        {
+            base.SelectionChanged(selectedIds);
+            if (allItems != null)
+            {
+                var items = allItems.Where(x => selectedIds.Contains(x.id));
+                var objects = items.Select(x => x.gameObject).ToArray();
+                UnityEditor.Selection.objects = objects;
+            }
+        }
+    }
+} 
---
+++ PrefabBrowser/Editor/PrefabBrowserWindow.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+namespace HypnagogiaGames.PrefabBrowser
+{
+    namespace Assets.Code.Editor
+    {
+        public class PrefabBrowserWindow : EditorWindow
+        {
+            // SerializeField is used to ensure the view state is written to the window 
+            // layout file. This means that the state survives restarting Unity as long as the window
+            // is not closed. If the attribute is omitted then the state is still serialized/deserialized.
+            [SerializeField] TreeViewState m_TreeViewState;
+
+            //The TreeView is not serializable, so it should be reconstructed from the tree data.
+            PrefabBrowserTreeView m_SimpleTreeView;
+
+            GameObject targetObject;
+            public GameObject TargetObject
+            {
+                get { return targetObject; }
+                set { targetObject = value; }
+            }
+
+            [MenuItem("Window/Hypnagogia Games/Prefab Browser")]
+            static void ShowWindow()
+            {
+                var window = GetWindow<PrefabBrowserWindow>();
+                window.titleContent = new GUIContent("Prefab Browser");
+                window.Show();
+            }
+
+            void OnEnable()
+            {
+                // Check whether there is already a serialized view state (state 
+                // that survived assembly reloading)
+                if (m_TreeViewState == null)
+                    m_TreeViewState = new TreeViewState();
+
+                m_SimpleTreeView = new PrefabBrowserTreeView(m_TreeViewState);
+            }
+
+            void OnGUI()
+            {
+                TargetObject = (GameObject)EditorGUI.ObjectField(new Rect(3, 3, position.width - 6, 20), "Target Object", TargetObject, typeof(GameObject), false);
+                m_SimpleTreeView.SetTarget(TargetObject);
+                m_SimpleTreeView.OnGUI(new Rect(0, 20, position.width, position.height));
+            }
+        }
+    }
+}
